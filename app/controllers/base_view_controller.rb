c5d2ec1e8f61e2b2b4ff3e100d9bc30222c8a622
seanlilmateus <seanlilmateus@yahoo.de>
2015-03-30 19:18:01 +0200

    added the base ViewController, subview of NSViewController that responds on #new

---
+++ app/controllers/base_view_controller.rb
@@ -0,0 +1,19 @@
+module Carendar
+    class BaseViewController < NSViewController
+        def init
+            initWithNibName(nil, bundle:nil)
+        end
+        
+        def loadView
+            self.view = NSView.alloc.init
+        end
+        
+        def viewDidLoad
+            super
+        end
+        
+        def updateViewConstraints
+            super
+        end
+    end
+end
