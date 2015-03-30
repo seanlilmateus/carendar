fd4f47fe68dfa2e146c702f86fcc5878feb8af49
seanlilmateus <seanlilmateus@yahoo.de>
2015-03-30 21:14:14 +0200

    added the content view controller for the NSPopview

---
+++ app/controllers/content_view_controller.rb
@@ -0,0 +1,106 @@
+module Carendar
+  class ContentViewController < BaseViewController
+    
+    def loadView
+      self.view = NSView.alloc.initWithFrame([[0, 0], [280.0, 600]])
+      self.view.wantsLayer = true
+      self.view.extend(Layout::View)
+    end
+    
+    def viewDidLoad
+      super
+      @content_delegate = ContentDelegate.new WeakRef.new(self)
+      # attache the childrean Controllers
+      addChildViewController calendar_view_controller
+      addChildViewController events_view_controller
+      # add Subviews
+      self.view.addSubview calendar_view_controller.view
+      self.view.addSubview events_view_controller.view
+      self.view.addSubview today_button
+      self.view.addSubview settings_button
+    end
+    
+    def viewDidLayout
+      @content_delegate.content_loaded
+    end
+    
+    def updateViewConstraints
+      super
+      Dispatch.once do
+        calendar = calendar_view_controller.view
+        table_view = events_view_controller.view
+        self.view.addConstraints([
+          calendar.top == self.view.leading - 160,
+          calendar.centerX == self.view.centerX,
+          calendar.width == self.view.width,
+          
+          today_button.centerX == self.view.centerX,
+          today_button.top(250) == calendar.bottom + 5,
+          #### Settings Button
+          settings_button.centerY == today_button.centerY,
+          settings_button.height == today_button.height,
+          settings_button.right == self.view.right - 5,
+          settings_button.width == today_button.width,
+          
+          table_view.centerX == self.view.centerX,
+          table_view.width == self.view.width,
+          table_view.top == today_button.bottom + 10,
+          table_view.bottom == self.view.bottom,
+          table_view.height(250) == self.view.height / 2.4,
+        ])
+      end
+    end
+    
+    # childrean Controllers
+    def calendar_view_controller
+      @__calendar_view_controller__ ||= CalendarController.new.tap do |ins|
+        inst.delegate = @content_delegate
+        inst.view.extend(Layout::View)
+      end
+    end
+    
+    def events_view_controller
+      @__events_view_controller ||= EventsViewController::new.tap do |evc|
+        evc.view.translatesAutoresizingMaskIntoConstraints = false
+        evc.view.extend(Layout::View)
+      end
+    end
+    
+    ## Events Fetcher Promise based
+    def events_fetcher
+      @__events_fetcher ||= EventsFetcher.new
+    end
+    
+    # UI Components
+    def today_button
+      @__today_button__ ||= create_button do |b|
+        b.title = localized_string("Today", "Today")
+        b.target = @content_delegate
+        b.action = 'select_date:'
+      end
+    end
+    
+    def settings_button
+      @__settings_button__ ||= create_button do |b|
+        button.imagePosition = NSImageOnly
+        button.buttonType = NSMomentaryChangeButton
+        button.alternateImage = NSImage.imageNamed('settings')
+        button.image = NSImage.imageNamed('settings-active')
+        button.bordered = false
+        button.target = @content_delegate
+        button.action = 'show_menu:'
+      end
+    end
+    
+    private def create_button
+      NSButton.alloc.init.tap do |b|
+        b.translatesAutoresizingMaskIntoConstraints = false
+        b.bezelStyle = NSTextFieldRoundedBezel
+        b.buttonType = NSMomentaryPushInButton
+        yield(b) if block_given?
+        b.extend(Layout::View)
+      end
+    end
+    
+  end
+end
---
+++ app/controllers/delegates/content_delegate.rb
@@ -0,0 +1,128 @@
+module Carendar
+  class ContentDelegate
+    
+    def initialize(controller)
+      @controller = controller
+    end
+    
+    def content_loaded
+      controller.today_button.enabled = false
+      events_for_the_month(controller.date)
+      update_empty_view
+    end
+    
+    def didChangeMonth(date)
+      update_empty_view
+      deselect_rows
+      controller.today_button.enabled = current_month?
+    end
+    
+    def didSelectDate(date)
+      update_empty_view date_formatter.stringFromDate(date)
+      if date.nil?
+        didChangeMonth(calendar_controller.date)
+        return
+      end
+      events_for_the_day(date)
+    end
+    
+    # Buttons Actions
+    def select_date(sender) # previous go to date
+      controller.date = NSDate.date
+      sender.enabled = false
+      events_for_the_month(controller.calendar_view_controller.date)
+      deselect_rows(sender)
+    end
+    
+    def show_menu(sender)
+      if sender
+        app_name = NSApp.delegate.send(:app_name)
+        about_string = localized_string("About %@", "About %@")
+        quit_string = localized_string("Quit %@", "Quit %@")
+        show_setting = localized_string("Preferences", "Preferences")
+        
+        menu = NSMenu.new.tap { |m| m.autoenablesItems = true }
+        [
+          { action: 'show_about_screen:', title: localized(about_string, app_name), eq:''},
+          { action: 'show_settings:', title: show_setting, eq:','},
+          { action: 'quit_application:', title: localized(quit_string, app_name), eq:'q'},
+        ].each do |h|
+          item = menu.addItemWithTitle(h[:title], action:h[:action], keyEquivalent:h[:eq])
+          item.target = NSApp.delegate
+          item.action = h[:action]
+        end        
+        menu.insertItem(NSMenuItem.separatorItem, atIndex:2)
+        
+        event = create_nsevent(sender)
+        NSMenu.popUpContextMenu(menu, withEvent:event, forView:sender)
+      end
+    end
+    
+    private
+    
+    attr_reader :controller
+    
+    def current_month?
+      controller.calendar_view_controller
+                .view
+                .subviews
+                .select { |sbv| sbv.is_a?(CalendarCell) }
+                .none?(&:today?)
+    end
+    
+    def date_formatter
+      # dafault short date format from somewhere???
+      @__date_formatter__ ||= NSDateFormatter.new.tap do |df|
+        df.timeStyle = NSDateFormatterNoStyle
+        df.dateStyle = NSDateFormatterFullStyle
+        df.locale = NSLocale.autoupdatingCurrentLocale
+      end
+    end
+    
+    def deselect_rows(sender=nil)
+      table_view = controller.events_view_controller.tableView
+      calendar = controller.calendar_view_controller.view
+      calendar.subviews
+              .select { |sbv| sbv.is_a?(CalendarCell) }
+              .each   { |sbv| sbv.selected = false }
+      table_view.deselectAll(sender)
+    end
+    
+    def update_empty_view(date_string=controller.calendarTitle.stringValue)
+      table_view = controller.events_view_controller.tableView
+      table_view.date_label.stringValue = date_string
+    end
+    
+    def events_for_the_month(date)
+      events_controller = controller.events_view_controller
+      controller.events_fetcher
+                .events_of_the_month(date)
+                .then { |items| events_controller.events = items }
+                .then { events_controller.reload_data }
+    end
+    
+    def events_for_the_day(date)
+      events_controller = controller.events_view_controller
+      controller.events_fetcher
+                .events_of_the_day(date)
+                .then { |items| events_controller.events = items }
+                .then { events_controller.reload_data }
+    end
+    
+    def create_nsevent(sender)
+      frame = sender.frame
+      pt = NSPoint.new(frame.origin.x + 120, frame.origin.y - 5)
+      menu_origin = sender.superview.convertPoint(pt, toView:nil)
+      NSEvent.mouseEventWithType( NSLeftMouseDown,
+                        location: menu_origin,
+                   modifierFlags: NSLeftMouseDownMask,
+                       timestamp: 1.0,
+                    windowNumber: sender.window.windowNumber,
+                         context: sender.window.graphicsContext,
+                     eventNumber: 0,
+                      clickCount: 1,
+                        pressure: 1)
+    end
+    
+  end
+end
