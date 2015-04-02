ea145431f74c93af518d320537ae5cfe9012b7bc
seanlilmateus <seanlilmateus@yahoo.de>
2015-04-02 12:40:38 +0200

    refactored the EventsFetcher to use DateOffset Object to create the date range for Month & Days

--- app/controllers/data_sources/events_fetcher.rb
+++ app/controllers/data_sources/events_fetcher.rb
@@ -1,26 +1,23 @@
 module Carendar
   class EventsFetcher
-    
+    def initialize
+      @storage = EKEventStore.new
+    end
     def events_of_the_day(date)
-      validate_date?(date).then { allowed? }
-                          .then do
-                            start_date = beginning_of_day(date)
-                            end_date = end_of_day(start_date)
-                            events_from(start_date, to:end_date)
-                          end
-
+      allowed?.then { validate_date?(date) }
+              .then { DateOffset.new(date, DateOffset::DAY, 1) }
+              .then { |offset| events_from(offset.start_date, to:offset.end_date) }
     end
     
     def events_of_the_month(date)
-      validate_date?(date).then { allowed? }
-                          .then do
-                            start_date = beginning_of_mounth(date)
-                            end_date = end_of_mounth(date)
-                            events_from(start_date, to:end_date)
-                          end
+      allowed?.then { validate_date?(date) }
+              .then { DateOffset.new(date, DateOffset::MONTH, 1) }
+              .then { |offset| events_from(offset.start_date, to:offset.end_date) }
     end
     
     private
+    
+    attr_reader :storage
     
     def events_from(start_date, to:end_date)
       predicate = storage.predicateForEventsWithStartDate( start_date,
@@ -29,8 +26,16 @@ module Carendar
       storage.eventsMatchingPredicate(predicate)
     end
     
-    def storage
-      @__storage__ ||= EKEventStore.new
+    def validate_date?(date)
+      promise = Promise.new
+      if date.is_a?(NSDate)
+        promise.fulfill(date)
+      else
+        info = { NSLocalizedDescriptionKey => "Invalid Date" }
+        error = NSError.errorWithDomain("Carendar", code:-5, userInfo:info)
+        promise.reject(error)
+      end
+      promise
     end
     
     def allowed?
@@ -41,46 +46,6 @@ module Carendar
       type = EKEntityTypeEvent # | EKEntityTypeReminder
       storage.requestAccessToEntityType(type, completion:completion)
       promise
-    end
-    
-    def beginning_of_day(date)
-      calendar = CalendarController.calendar
-      opts = NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit
-      components = calendar.components(opts, fromDate:date)
-      calendar.dateFromComponents(components)
-    end
-    
-    def end_of_day(date)
-      calendar = CalendarController.calendar
-      components = NSDateComponents.new
-      components.day = 1
-      date = calendar.dateByAddingComponents(components, toDate:date, options:0)
-      date.dateByAddingTimeInterval(-1)
-    end
-    
-    def beginning_of_month(date)
-      calendar = CalendarController.calendar
-      opts = NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit
-      components = calendar.components(opts, fromDate:date)
-      components.day = 1
-      beginning_of_day calendar.dateFromComponents(components)
-    end
-    
-    def end_of_month(date)
-      calendar = CalendarController.calendar
-      opts = NSYearCalendarUnit|NSMonthCalendarUnit|NSWeekCalendarUnit|NSWeekdayCalendarUnit
-      comps = calendar.components(opts, fromDate:date)
-      # set last of month
-      comps.month = comps.month + 1
-      comps.day = 0      
-      end_of_day calendar.dateFromComponents(comps)
-    end
-    
-    def validate_date?(date)
-      promise = Promise.new
-      date.nil? ? promise.reject : promise.fulfill
-      promise
-    end
-    
+    end    
   end
 end
