a0bedde1c936c6783c9f74935c8cf60dc1a3beb8
seanlilmateus <seanlilmateus@yahoo.de>
2015-04-07 11:27:50 +0200

    TableView Controller
    this is our events controller the main view is our custom NSTableView
    subclass..
    EventsTableView

---
+++ app/controllers/events_view_controller.rb
@@ -0,0 +1,39 @@
+module Carendar
+  class EventsViewController < BaseViewController
+    
+    def init
+      super.tap { @data_source = EventsDataSource.new }
+    end
+    attr_reader :data_source
+    
+    def loadView
+      self.view = table_container
+    end
+    
+    def viewDidLoad
+      super
+      self.tableView.delegate = @data_source
+      self.tableView.dataSource = @data_source
+    end
+    
+    def tableView
+      @__tableView__ ||= EventsTableView.new NSRect.new([0, 0], [280.0, 200])
+    end
+    
+    private
+    def table_container
+      @__table_container__ ||= begin
+        rect = NSRect.new([0, 0], [280.0, 250.0])
+        scrollview = NSScrollView.alloc.initWithFrame(rect).tap do |scv|
+          scv.backgroundColor = NSColor.clearColor
+          scv.contentView.backgroundColor = NSColor.clearColor
+          scv.documentView = tableView
+          scv.hasHorizontalScroller = false
+          scv.horizontalLineScroll = 0.0
+          scv.drawsBackground = true
+        end
+      end
+    end
+    
+  end
+end
