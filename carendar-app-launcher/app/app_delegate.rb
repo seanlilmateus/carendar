daa65f91f47acde9ef0b22622dfce6b7525f7cf0
seanlilmateus <seanlilmateus@yahoo.de>
2015-10-18 21:04:17 +0200

    Finally restarting correctly
    Restart after reboot

--- carendar-app-launcher/app/app_delegate.rb
+++ carendar-app-launcher/app/app_delegate.rb
@@ -1,16 +1,17 @@
 class AppDelegate
   DEST_BUNDLE_ID = "de.mateus.Carendar"
   def applicationDidFinishLaunching(notification)
-    NSLog "Failed to launch #{app_name} app" unless start_app(DEST_BUNDLE_ID)
-    NSApp.performSelector("terminate:", withObject: nil, afterDelay: 0.0)
+    start_app(DEST_BUNDLE_ID)
   end
   
   private
   def start_app(id)
     wsp = NSWorkspace.sharedWorkspace
-    return if ws.runningApplications.map(&:bundleIdentifier).include?(DEST_BUNDLE_ID)
-    opts = NSWorkspaceLaunchAsync
-    wsp.launchAppWithBundleIdentifier(id, options:opts, additionalEventParamDescriptor:nil, launchIdentifier:nil)
+    unless wsp.runningApplications.map(&:bundleIdentifier).include?(DEST_BUNDLE_ID)
+      opts = NSWorkspaceLaunchAsync
+      wsp.launchAppWithBundleIdentifier(id, options:opts, additionalEventParamDescriptor:nil,launchIdentifier:nil)
+    end
+    NSApp.performSelector("terminate:", withObject: nil, afterDelay: 0.0)
   end
 end
 
