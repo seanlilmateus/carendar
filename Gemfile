0a708d3288510efdcf9f99a3c48cd28c26e54ec0
seanlilmateus <seanlilmateus@yahoo.de>
2016-06-04 12:08:37 +0200

    Promise.rb update '0.7.0'
    we updated to the lastest Promise.rb version 0.70, unfortunately
    Rubymotion doesn’t support Module#private_constant, we have to clone it
    and locally comment out any line with Module#private_constant.
    
    Since it also implements its own Promise#catch, we don’t need it
    anymore.
    
    And Promise.after is always execute on default concurrent Queue

--- Gemfile
+++ Gemfile
@@ -5,6 +5,6 @@ gem 'rake'
 gem 'motion-bundler'
 gem 'WithMotion'
 group :motion do
-  gem 'promise.rb'
+  gem 'promise.rb', :path => '../promise.rb'
 end
 # Add your dependencies here:
--- app/helpers/promise_extension.rb
+++ app/helpers/promise_extension.rb
@@ -1,21 +1,16 @@
 class Promise
 
-  def self.after(delay, queue=Dispatch::Queue.main)
+  def self.after(delay, queue=Dispatch::Queue.concurrent)
     subject = Promise.new
     queue.after(delay) { subject.fulfill(delay) }
     subject
   end
 
 
-  def catch(&block)
-    self.then(nil, block.to_proc)
-  end
-
-
   def on_queue(queue=Dispatch::Queue.concurrent)
     subject = Promise.new
     self.then  { |*items| queue.async { subject.fulfill(*items) } }
-    .catch { |*items| queue.async { subject.reject(*items)  } } 
+        .catch { |*items| queue.async { subject.reject(*items)  } } 
     subject
   end
 
