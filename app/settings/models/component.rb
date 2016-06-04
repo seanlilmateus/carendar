44537c10994fad660a1d523a72e6f4e202ea0cc2
seanlilmateus <seanlilmateus@yahoo.de>
2016-06-04 12:02:55 +0200

    Better Support fur NSSecureCoding
    According to the Documentation the Encoder has to use an NSSet with
    Container Class and the class of the items containing in the Array. in
    our case [NSArray and NSString].
    If the Object is a Container on anything

--- app/settings/models/component.rb
+++ app/settings/models/component.rb
@@ -7,6 +7,11 @@ module Carendar
       end
 
 
+      def self.supportsSecureCoding
+        true
+      end
+
+
       def initialize(name = nil, type = nil, items = [], index = 0)
         self.name, self.type, self.index = name, type, index
         #timezone = NSTimeZone.timeZoneWithAbbreviation()
@@ -17,9 +22,10 @@ module Carendar
 
       def initWithCoder(decoder)
         self.tap do |instance|
+          klasses = NSSet.setWithArray([NSArray, NSString])
           instance.name = decoder.decodeObjectOfClass(NSString, forKey: "name")
           instance.type = decoder.decodeObjectOfClass(NSString, forKey: "type")
-          instance.attributes = decoder.decodeObjectOfClass(NSArray, forKey: "attributes")
+          instance.attributes = decoder.decodeObjectOfClasses(klasses, forKey: "attributes")
           instance.index = decoder.decodeObjectOfClass(NSNumber, forKey: "index") || 0
         end
       end
